948e266e854716a389b6b17af104fe3b917e0f2c
kl3inIT <dathip04@gmail.com>
2025-07-28 17:57:11 +0700

    working with data in ui

--- src/main/java/com/example/jmix/view/user/UserDetailView.java
+++ src/main/java/com/example/jmix/view/user/UserDetailView.java
@@ -1,15 +1,22 @@
 package com.example.jmix.view.user;
 
 import com.example.jmix.entity.OnboardingStatus;
+import com.example.jmix.entity.Step;
 import com.example.jmix.entity.User;
+import com.example.jmix.entity.UserStep;
 import com.example.jmix.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.model.CollectionPropertyContainer;
+import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +41,13 @@ public class UserDetailView extends StandardDetailView<User> {
     private ComboBox<String> timeZoneField;
     @ViewComponent
     private MessageBundle messageBundle;
+
+    @ViewComponent
+    private DataContext dataContext;
+
+    @ViewComponent
+    private CollectionPropertyContainer<UserStep> stepsDc;
+
     @Autowired
     private Notifications notifications;
 
@@ -41,6 +55,10 @@ public class UserDetailView extends StandardDetailView<User> {
     private EntityStates entityStates;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private DataManager dataManager;
+
 
     @Subscribe
     public void onInit(final InitEvent event) {
@@ -81,6 +99,33 @@ public class UserDetailView extends StandardDetailView<User> {
                     .withType(Notifications.Type.WARNING)
                     .withPosition(Notification.Position.TOP_END)
                     .show();
+        }
+    }
+
+    @Subscribe(id = "generateButton", subject = "clickListener")
+    public void onGenerateButtonClick(final ClickEvent<JmixButton> event) {
+        User user = getEditedEntity();
+
+        if (user.getJoiningDate() == null) {
+            notifications.create("Cannot generate steps for user without 'Joining date'")
+                    .show();
+            return;
+        }
+
+        List<Step> steps = dataManager.load(Step.class)
+                .query("select s from Step s order by s.sortValue asc")
+                .list();
+
+        for (Step step : steps) {
+            if (stepsDc.getItems().stream().noneMatch(userStep ->
+                    userStep.getStep().equals(step))) {
+                UserStep userStep = dataContext.create(UserStep.class);
+                userStep.setUser(user);
+                userStep.setStep(step);
+                userStep.setDueDate(user.getJoiningDate().plusDays(step.getDuration()));
+                userStep.setSortValue(step.getSortValue());
+                stepsDc.getMutableItems().add(userStep);
+            }
         }
     }
 }
